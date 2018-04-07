0b85f2df49746c2eb531e43c54695689c212eed0
Ryan Breault <ryan.breault@Ryan-Breault-Macbook-2.local>
2018-04-07 10:53:49 -0500

    fixed contact form and carousel

---
+++ contactthanks.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>A Nice & Simple Contact Form</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/style.css" />
+	<link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Lato:300,300i,700" rel="stylesheet">
+
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+
+		<br /><br />
+
+
+		<h1>Your message has been sent!</h1><br />
+
+		<p><a href="index.html">Back to Site</a></p>
+
+	</div>
+
+</body>
+
+</html>
